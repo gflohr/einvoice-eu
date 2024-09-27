11e98ccdb5bf29120a1e032451dda10768927560
Guido Flohr <guido.flohr@cantanea.com>
2024-09-28 00:22:50 +0300

    add utility module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UtilityModule } from './utility/utility.module';
 
 @Module({
-	imports: [],
+	imports: [UtilityModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
