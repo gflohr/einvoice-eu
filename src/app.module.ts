542b62b54368402bcc5ed3a3cda3fd6c529cfc88
Guido Flohr <guido.flohr@cantanea.com>
2024-09-29 17:26:24 +0300

    do not need date parsing service for now

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UtilityModule } from './utility/utility.module';
 
 @Module({
-	imports: [UtilityModule],
+	imports: [],
 	controllers: [AppController],
 	providers: [AppService],
 })
--- src/utility/utility.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DateParserService } from './date-parser.service';
-
-@Module({
-	providers: [DateParserService],
-})
-export class UtilityModule {}
